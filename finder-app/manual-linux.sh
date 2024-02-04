5ff9237167203b72b4259537f6ec779161a9c602
rojasx <rojasx@colorado.edu>
2024-02-03 18:09:34 -0700

    wip, kernel panic error

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -21,7 +21,7 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+mkdir -p "${OUTDIR}"
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -35,11 +35,19 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+    
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
+
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
@@ -47,34 +55,53 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+echo "Creating rootfs!"
+
+cd "$OUTDIR"
+mkdir -p "rootfs"
+cd "rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    echo "Getting busybox!"
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+echo "Building busybox!"
+
+make distclean
+make defconfig
+echo "Defconfig completed!"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+echo "Build completed!"
+make CONFIG_PREFIX=/tmp/aeld/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+echo "Install completed!"
 
 echo "Library dependencies"
+
+cd "${OUTDIR}/rootfs"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+echo "Making device nodes!"
 
-# TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+echo "Making and zipping writer utility!"
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+find . | cpio -H newc -ov --owner root:root > /tmp/aeld/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
+
 
 # TODO: Chown the root directory
 
-# TODO: Create initramfs.cpio.gz
