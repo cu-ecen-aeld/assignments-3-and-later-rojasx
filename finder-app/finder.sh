50ceaf4143600716802c68cd82d0a09534f00c5c
rojasx <rojasx@colorado.edu>
2024-02-04 00:14:32 -0700

    converted from bash to sh script.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 # Will use this multiple times
 help_statement="Usage: finder.sh <filesdir> <searchstr>"
 
-# Check if there's any arguments at all
-if [ "$#" == 0 ]; then
+# Check if there are any arguments at all
+if [ "$#" = 0 ]; then
     echo -e "error: no arguments.\n$help_statement"
     exit 1
 # Check if there's no second argument
-elif [ "$2" == "" ]; then
+elif [ "$2" = "" ]; then
     echo -e "error: bad arguments.\n$help_statement"
     exit 1
-# Check if there filesdir exists
+# Check if the filesdir exists
 elif [ ! -d "$1" ]; then
     echo -e "error: <filesdir> provided does not exist or is not a dir.\n$help_statement"
     exit 1
@@ -24,11 +24,9 @@ search_output=$(find "$filesdir" -type f -exec grep "$searchstr" {} +)
 
 # Count the number of files in the dir
 x=$(find "$filesdir" -type f | wc -l)
-# x=$(echo "$search_output" | cut -d: -f1 | sort -u | wc -l)
 
 # Count the total number of matching lines (y)
-y=$(wc -l <<< "$search_output")
-# y=$(echo "$search_output" | wc -l)
+y=$(echo "$search_output" | wc -l)
 
 # Output the results
 echo "The number of files are $x and the number of matching lines are $y"
