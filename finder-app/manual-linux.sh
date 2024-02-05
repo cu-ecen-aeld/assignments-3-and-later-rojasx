28ae371b91e2b7ae8255c5f9129fa411462d8ac8
rojasx <rojasx@colorado.edu>
2024-02-04 21:28:16 -0700

    putting everything back

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -15,127 +15,118 @@ CROSS_COMPILE=aarch64-none-linux-gnu-
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 echo "${SYSROOT}"
 
-# if [ $# -lt 1 ]
-# then
-# 	echo "Using default directory ${OUTDIR} for output"
-# else
-# 	OUTDIR=$1
-# 	echo "Using passed directory ${OUTDIR} for output"
-# fi
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
+else
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
+fi
 
-# mkdir -p "${OUTDIR}"
+mkdir -p "${OUTDIR}"
 
-# cd "$OUTDIR"
-# ############################################
-# # Get linux
+cd "$OUTDIR"
+############################################
+# Get linux
 
-# if [ ! -d "${OUTDIR}/linux-stable" ]; then
-#     #Clone only if the repository does not exist.
-# 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-# 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-# else
-#     echo "Already has linux-stable"
-# fi
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+else
+    echo "Already has linux-stable"
+fi
 
-# if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-#     cd linux-stable
-#     echo "Checking out version ${KERNEL_VERSION}"
-#     git checkout ${KERNEL_VERSION}
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
 
-#     # Kernel build steps
-#     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-#     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-#     make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-#     # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-#     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
-# else
-#     echo "Already has a valid Image"
-# fi
-
-# ############################################
-# echo "Adding the Image in outdir"
-    
-# cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
-
-# ############################################
-# echo "Creating the staging directory for the root filesystem"
-
-# cd "$OUTDIR"
-# if [ -d "${OUTDIR}/rootfs" ]
-# then
-# 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-#     sudo rm  -rf ${OUTDIR}/rootfs
-# fi
-
-# ############################################
-# echo "Creating rootfs!"
-
-# cd "$OUTDIR"
-# mkdir -p "rootfs"
-# cd "rootfs"
-# mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-# mkdir -p usr/bin usr/lib usr/sbin
-# mkdir -p var/log
-
-# ############################################
-# # Busybox
-# cd "$OUTDIR"
-# if [ ! -d "${OUTDIR}/busybox" ]
-# then
-#     echo "Getting busybox!"
-#     git clone git://busybox.net/busybox.git
-#     cd busybox
-#     git checkout ${BUSYBOX_VERSION}
-# else
-#     cd busybox
-# fi
-
-# ############################################
-# echo "Building busybox!"
-
-# make distclean
-# make defconfig
-# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-# make CONFIG_PREFIX=/tmp/aeld/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
-
-# ############################################
-# echo "Library dependencies"
-
-# # cd "${OUTDIR}/rootfs/bin"
-# # sudo chown -R root:root *
-# # cd "${OUTDIR}/rootfs"
-# # ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-# # ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-
-# echo "Copying libraries from arm-cross-compiler"
-# echo "${SYSROOT}"
-# cp -a "${SYSROOT}/lib/ld-linux-aarch64.so.1" "${OUTDIR}/rootfs/lib/"
-# cp -a "${SYSROOT}/lib64/libm.so.6" "${OUTDIR}/rootfs/lib64/"
-# cp -a "${SYSROOT}/lib64/libresolv.so.2" "${OUTDIR}/rootfs/lib64/"
-# cp -a "${SYSROOT}/lib64/libc.so.6" "${OUTDIR}/rootfs/lib64/"
+    # Kernel build steps
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+else
+    echo "Already has a valid Image"
+fi
 
 ############################################
-# Just for moving assgn files into rootfs before compressing
+echo "Adding the Image in outdir"
+    
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+
+############################################
+echo "Creating the staging directory for the root filesystem"
+
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
+then
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf ${OUTDIR}/rootfs
+fi
+
+############################################
+echo "Creating rootfs!"
+
+cd "$OUTDIR"
+mkdir -p "rootfs"
+cd "rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
+############################################
+# Busybox
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+    echo "Getting busybox!"
+    git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+else
+    cd busybox
+fi
+
+############################################
+echo "Building busybox!"
+
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=/tmp/aeld/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+############################################
+echo "Moving library dependencies!"
+
+# cd "${OUTDIR}/rootfs/bin"
+# sudo chown -R root:root *
+# cd "${OUTDIR}/rootfs"
+# ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+# ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
+echo "Copying libraries from arm-cross-compiler!"
+echo "${SYSROOT}"
+cp -a "${SYSROOT}/lib/ld-linux-aarch64.so.1" "${OUTDIR}/rootfs/lib/"
+cp -a "${SYSROOT}/lib64/libm.so.6" "${OUTDIR}/rootfs/lib64/"
+cp -a "${SYSROOT}/lib64/libresolv.so.2" "${OUTDIR}/rootfs/lib64/"
+cp -a "${SYSROOT}/lib64/libc.so.6" "${OUTDIR}/rootfs/lib64/"
+
+############################################
+# Creating writer
+
+echo "Cross compiling!"
+
+cd $FINDER_APP_DIR
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
-echo "Moving assignment files"
+############################################
+# Just for moving assgn files into rootfs before compressing
 
-# debug stuff
-echo
-echo
-echo "SYSROOT is"
-echo "${SYSROOT}"
-echo "FINDER APP DIR is:"
-echo $FINDER_APP_DIR
-echo "Working dir is:"
-echo $PWD 
-echo "My contents are:"
-ls
-echo
-echo
-
-# debug stuff
+echo "Moving assignment files!"
 
 cp "${FINDER_APP_DIR}/writer" "${OUTDIR}/rootfs/home/"
 cp "${FINDER_APP_DIR}/finder.sh" "${OUTDIR}/rootfs/home/"
@@ -150,7 +141,7 @@ sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
 ############################################
-echo "Ownership of rootfs to root"
+echo "Ownership of rootfs to root!"
 
 cd "${OUTDIR}/rootfs"
 sudo chown -R root:root * 
@@ -162,3 +153,5 @@ cd "${OUTDIR}/rootfs"
 find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
 cd "$OUTDIR"
 gzip -f initramfs.cpio
+
+echo "Finished!!!"
